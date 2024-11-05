284573c94a282d3a23f4ff2778949417b7500a23
Lucas Do Nascimento Rosa <143842123+lucasrosa6@users.noreply.github.com>
2024-11-05 08:48:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Ordem Paranormal',
+    'O que é "Ordem Paranormal"?',
+    'É um RPG de Mesa'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Quantas temporadas tem "Ordem Paranormal" ?',
+    'O RPG tem 6 temporadas'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Qual é o objetivo principal da Ordo Realitas?',
+    'Acabar com os planos de Kian'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Em que lugar do mundo está fixada a base da Ordo Realitas?',
+    'Está fixada em São Paulo no bar do Suvaco Seco no Brasil'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'O que são Tokens?',
+    'São os bonecos que são controlados pelo jogadores'
+)
+
+criaCartao(
+    'Ordem Paranormal',
+    'Quem é o líder da Ordo Realista? (até a data do lançamento deste site)',
+    'O Senhor Veríssimo'
+)
